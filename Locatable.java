ae48f79ea06ef99a69825ba52e04433c1b3e6fbf
emrekaratas22001641 <emrekaratas22001641@gmail.com>
2021-07-01 17:16:28 +0300

    Locatable is done

---
+++ Locatable.java
@@ -0,0 +1,13 @@
+/**
+ * Interface Locatable
+ * @author Emre Karatas-22001641
+ * @version v1.0 -30.06.2021
+ */
+public interface Locatable
+{
+    double getX();
+    double getY();
+
+    void setLocation(double x, double y);
+
+}
